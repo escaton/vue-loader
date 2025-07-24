eec08e46346d5f95ce60dfe4c0a7175c316b4267
Egor Blinov <egor.b@miro.com>
2025-07-25 00:23:25 +0200

    feat: support multiple css-modules imports

--- src/cssModules.ts
+++ src/cssModules.ts
@@ -1,3 +1,13 @@
+export function genInitCSSModulesCode() {
+  return `
+const cssModules = {}
+const cssModulesStore = {}
+const getCssModules = (name) => {
+  return Object.values(cssModulesStore[name]).reduce((acc, style) => Object.assign(acc, style), {})
+}
+`
+}
+
 export function genCSSModulesCode(
   id: string,
   index: number,
@@ -6,17 +16,24 @@ export function genCSSModulesCode(
   needsHotReload: boolean
 ): string {
   const styleVar = `style${index}`
-  let code = `\nimport ${styleVar} from ${request}`
-
   // inject variable
   const name = typeof moduleName === 'string' ? moduleName : '$style'
-  code += `\ncssModules["${name}"] = ${styleVar}`
+  let code = `
+import ${styleVar} from ${request}
+
+if (!cssModulesStore["${name}"]) {
+  cssModulesStore["${name}"] = {}
+}
+cssModulesStore["${name}"]["${styleVar}"] = ${styleVar}
+cssModules["${name}"] = getCssModules("${name}")
+`
 
   if (needsHotReload) {
     code += `
 if (module.hot) {
   module.hot.accept(${request}, () => {
-    cssModules["${name}"] = ${styleVar}
+    cssModulesStore["${name}"]["${styleVar}"] = ${styleVar}
+    cssModules["${name}"] = getCssModules("${name}")
     __VUE_HMR_RUNTIME__.rerender("${id}")
   })
 }`
---
+++ test/fixtures/css-modules-multiple.vue
@@ -0,0 +1,14 @@
+<style module>
+.red {
+  color: red;
+}
+</style>
+<style module>
+.green {
+  color: green;
+}
+</style>
+
+<script>
+export default {}
+</script>
